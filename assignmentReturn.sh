5fdfcb6f7857e647a08b4b5b8541c4bf6bfe8243
Karl R. Wurst <karl@w-sts.com>
2013-05-24 10:35:01 -0400

    Added licensing information

--- assignmentReturn.sh
+++ assignmentReturn.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+# Copyright (C) 2013 Karl R. Wurst
+# 
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
+
 # This script returns a graded student assignment to their repository.
 # It will leave the graded assignment in a new branch.
 # The student will have to merge the changes into their master branch
--- lab2FetchConvert.sh
+++ lab2FetchConvert.sh
@@ -1,5 +1,27 @@
 #!/bin/bash
 
+# Copyright (C) 2013 Karl R. Wurst
+# 
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301, USA
+
+# This script returns a graded student assignment to their repository.
+# It will leave the graded assignment in a new branch.
+# The student will have to merge the changes into their master branch
+# (or you have to issue a pull request, and maybe merge it depending on
+# how much you want to do for the students.)
+
 # This script will fetch a student assignment from Bitbucket for grading.
 # The files are converted to a single PDF and put into the grading folder.
 # Use the Lab2Return.sh script to return the graded PDF to the students.
