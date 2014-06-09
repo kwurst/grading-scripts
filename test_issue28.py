a25da32b7ffe5be1e9dbef4a72653d46a80a099e
Stoney Jackson <dr.stoney@gmail.com>
2014-06-09 15:04:22 -0400

    Add test for issue28

---
+++ test_issue28.py
@@ -0,0 +1,32 @@
+import unittest
+import sandbox
+import os.path
+from assignment import Assignment
+from command import Command
+
+
+OUTFILE = sandbox.dir('issue28.txt')
+
+
+class test_issue28(unittest.TestCase):
+
+    def test_missingFile_keepGoing(self):
+        collector = Collector()
+        Assignment(sandbox.dir('issue28.json')).accept(collector.visit)
+        lines = []
+        with open(OUTFILE) as outfile:
+            lines = outfile.readlines()
+        self.assertTrue(len(lines) == 5)
+
+
+class Collector(object):
+    def __init__(self):
+        self.ls = Command('ls "{ins}" >> ' + OUTFILE)
+
+    def visit(self, submission_directory, files_to_collect):
+        for file in files_to_collect:
+            self.ls(file)
+
+
+if __name__ == '__main__':
+    unittest.main()
